d663f1ed0ef33bf19d218bfef1fadeddb2b9237c
roberto <roberto.azevedo@visagio.com>
2019-04-02 12:53:28 -0300

    Arquivo config grafana

---
+++ README.txt
@@ -0,0 +1,5 @@
+Export:
+    Botao share (em cima) -> Export -> Save to file
+
+Import: 
+    Create (menu lateral) -> Import -> Upload .json File
--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express'),
-    port = process.env.port || 8080,
-    app = express()
-
-app.get('/', (req, res) => res.json({key: "value"}))
-
-app.listen(port, () => console.log(`Server start at ${port}`))
